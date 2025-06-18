8e077b50a6b153158d2e964ae587ef66e4305450
Ashish <ashish789pa@gmail.com>
2025-06-18 23:30:53 +0530

    chats rounds function and socket pending

--- src/Controller/messageControl.js
+++ src/Controller/messageControl.js
@@ -1,26 +1,27 @@
-const {conversationmodel} =require('../modules/conversation')
-const {messageSchema}=require('../modules/Message')
+const { conversationmodel } = require('../modules/conversation')
+const { messageSchema } = require('../modules/Message')
 
-exports.sendmessage= async  (req,res)=>{
-     try{
+exports.sendmessage = async (req, res) => {
+     try {
 
-          const SenderId=req.body;
-          const RecevierId=req.params.id
-          const {message}=req.body;
+          const { SenderId,RecevierId,message } =req.body;
 
-          let gotConversation= await conversationmodel.findOne({ participants: {$all: [SenderId,RecevierId] }})
-          if(!gotConversation){ gotConversation =await conversationmodel.create({ participants: {$all: [SenderId,RecevierId] }}) }
-          
+          if(!SenderId || !RecevierId || !message ) { return res.status(400).send({status:false, msg:`aal is  requires  `}) }
+
+          let gotConversation = await conversationmodel.findOne({ participants: { $all: [SenderId, RecevierId] } })
+          if (!gotConversation) { gotConversation = await conversationmodel.create({ participants: { $all: [SenderId, RecevierId] } }) }
+
           const newMessage = messageSchema.create({
                SenderId,
                RecevierId,
                message
           })
 
-          if(newMessage){ gotConversation.message.push(newMessage._id)  }
+          if (newMessage) { gotConversation.message.push(newMessage._id) }
           await gotConversation.save()
 
+          if (newMessage) { return res.status(400).send({ status: false, msg: 'message are required' }) }
 
-     }catch (e) {return res.status(500).send({ msg: e.message }); }
- 
+     } catch (e) { return res.status(500).send({ msg: e.message }); }
+
 }
